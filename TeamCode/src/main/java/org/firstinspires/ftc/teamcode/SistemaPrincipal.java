2455d56b0ffe763905e6e5b0ffa2f99c81bf1af8
Airyn BD <airyn.yojabed.alp@cbtis258.edu.mx>
2026-03-21 14:48:50 -0600

    Primer commit FTC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SistemaPrincipal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SistemaPrincipal.java
@@ -6,10 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
-public class sistema_principal extends LinearOpMode {
+public class SistemaPrincipal extends LinearOpMode {
 
     DcMotor Mi;
     DcMotor Md;
+    DcMotor Mr;
     DcMotorEx Mt;
 
     @Override
@@ -17,41 +18,49 @@ public class sistema_principal extends LinearOpMode {
 
         Mi = hardwareMap.get(DcMotor.class, "Mi");
         Md = hardwareMap.get(DcMotor.class, "Md");
+        Mr = hardwareMap.get(DcMotor.class, "Mr");
         Mt = hardwareMap.get(DcMotorEx.class, "Mt");
 
         Md.setDirection(DcMotor.Direction.REVERSE);
 
+        Mi.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Mi.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         Mt.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         Mt.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         Mt.setDirection(DcMotor.Direction.REVERSE);
+
+        Mr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Mr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Mr.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
             double avanzar = -gamepad1.left_stick_y;
-            double girar = gamepad1.left_stick_x;
+            double girar = gamepad1.right_stick_x;
 
             double potenciaIzquierda = avanzar + girar;
             double potenciaDerecha = avanzar - girar;
 
-            // limitar potencia entre -1 y 1
             potenciaIzquierda = Math.max(-1, Math.min(1, potenciaIzquierda));
             potenciaDerecha = Math.max(-1, Math.min(1, potenciaDerecha));
 
             Mi.setPower(potenciaIzquierda);
             Md.setPower(potenciaDerecha);
 
-            // motor disparador
-            if (gamepad1.a) {
-                Mt.setPower(1.0);
+            Mt.setPower(gamepad1.right_trigger);
+            if (gamepad1.left_bumper) {
+                Mr.setPower(1);
             } else {
-                Mt.setPower(0.0);
+                Mr.setPower(0);
             }
 
             telemetry.addData("Mi", potenciaIzquierda);
             telemetry.addData("Md", potenciaDerecha);
             telemetry.addData("Mt", Mt.getPower());
+            telemetry.addData("Mr", Mr.getPower());
             telemetry.update();
         }
     }
